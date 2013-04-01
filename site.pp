11db954b89de95cbaac6e5a5a6bc6b42fc039d7c
root <root@pies-mini.(none)>
2013-03-31 23:00:36 -0700

    added yeoman toolchain

--- site.pp
+++ site.pp
@@ -2,14 +2,7 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
 
-exec { "download-and-extract-sublimetext2-tar":
-  command => 'wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2; tar xf Sublime\ Text\ 2.0.1\ x64.tar.bz2',
-
-}
-
-exec { "move-sublime-executable-to-path":
-  command => 'mv Sublime\ Text\ 2 /opt/;ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime',
-  require => Exec["download-and-extract-sublimetext2-tar"],
-}
+#include nodejs_with_yeoman
+#include sublime_text_2
 
 
